0b13c52a6e253ab0eeae512041beb43b9db9c54a
complikatyed <katye.russell@gmail.com>
2016-05-27 20:43:03 -0500

    Add delete functionality

--- app/factories/ContactFactory.js
+++ app/factories/ContactFactory.js
@@ -3,7 +3,6 @@
 app.factory("contactStorage", function($q, $http, firebaseURL) {
 
   var getContactList = function() {
-
     let contacts = [];
 
     return $q(function(resolve, reject) {
@@ -11,10 +10,11 @@ app.factory("contactStorage", function($q, $http, firebaseURL) {
         // --- sends a 'get' call to the firebase database and grabs the object at that URL --- //
         .get(firebaseURL + "contacts.json")
         // --- on success, we now have access to the contacts data as an object --- //
-        .success(function(contactObject) {
-          console.log(contactObject);
-          var contactData = contactObject;
+        .success(function(objectFromFirebase) {
+          console.log(objectFromFirebase);
+          var contactData = objectFromFirebase;
           // --- Uses the Object.keys method to loop through the object --- //
+
           Object.keys(contactData).forEach(function(key) {
             // --- says "make the id for each object the same as that object's key" --- //
             contactData[key].id = key;
@@ -22,14 +22,32 @@ app.factory("contactStorage", function($q, $http, firebaseURL) {
             contacts.push(contactData[key]);
           });
           resolve(contacts);
-        }).error (function(error) {
+        })
+        .error (function(error) {
           reject(error);
         });
     });
-
   };
 
-  return {getContactList:getContactList};
+  var deleteContact = function(contactId){
+    return $q(function(resolve, reject){
+      $http
+        .delete(firebaseURL + "contacts/" + contactId + ".json")
+        .success(function(objectFromFirebase){
+          resolve(objectFromFirebase);
+        })
+        .error(function(error){
+          reject(error);
+      });
+    });
+  };
+
+
+
+
+
+
+  return {getContactList:getContactList, deleteContact:deleteContact};
 
 });
 
